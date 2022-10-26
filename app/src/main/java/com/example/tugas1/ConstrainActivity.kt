8ce6f740a1b364e73da608fc410d4fc380e7900a
acer <meisyareptiana29>
2022-10-26 10:31:35 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas1/ConstrainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugas1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstrainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constrain)
+    }
+}
